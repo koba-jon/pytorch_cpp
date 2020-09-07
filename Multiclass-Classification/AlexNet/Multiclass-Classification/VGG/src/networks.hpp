32f5f7655c05c53dfc4a0f866ce86bcb77abd231
koba-jon <56967584+koba-jon@users.noreply.github.com>
2020-09-07 10:54:59 +0900

    Add files via upload

---
+++ Multiclass-Classification/AlexNet/Multiclass-Classification/VGG/src/networks.hpp
@@ -0,0 +1,34 @@
+#ifndef NETWORKS_HPP
+#define NETWORKS_HPP
+
+#include <vector>
+// For External Library
+#include <torch/torch.h>
+#include <boost/program_options.hpp>
+
+// Define Namespace
+namespace nn = torch::nn;
+namespace po = boost::program_options;
+
+// Function Prototype
+void weights_init(nn::Module &m);
+nn::Sequential make_layers(const size_t nc, const std::vector<long int> cfg, const bool BN);
+
+
+// -------------------------------------------------
+// struct{MC_VGGImpl}(nn::Module)
+// -------------------------------------------------
+struct MC_VGGImpl : nn::Module{
+private:
+    nn::Sequential features, avgpool, classifier;
+public:
+    MC_VGGImpl(){}
+    MC_VGGImpl(po::variables_map &vm);
+    void init();
+    torch::Tensor forward(torch::Tensor x);
+};
+
+TORCH_MODULE(MC_VGG);
+
+
+#endif
