fefab8a498d51c8f8ef63d4bdb77bf02fb9d7501
koba-jon <koba37b8011@softbank.ne.jp>
2020-09-07 11:33:35 +0900

    Remove directory

--- Multiclass-Classification/AlexNet/Multiclass-Classification/VGG/datasets/set_CIFAR100.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 set_CIFAR100.py \
-    --output1_dir CIFAR100_org \
-    --output2_dir CIFAR100
--- Multiclass-Classification/AlexNet/Multiclass-Classification/VGG/datasets/set_MNIST.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 set_MNIST.py \
-    --output1_dir MNIST_org \
-    --output2_dir MNIST
