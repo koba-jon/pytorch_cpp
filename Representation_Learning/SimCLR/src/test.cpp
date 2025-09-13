1729fcd869bb1b0f8d01ae37e20e44edcb56f86f
Kobayashi Hiroki <koba@MacBook-Air-4.local>
2025-09-13 09:52:32 +0900

    Revert "Add files via upload"
    This reverts commit 926670c4b9799575f38b90ee02e21d0f1621646d.

--- Representation_Learning/SimCLR/src/test.cpp
+++ Representation_Learning/SimCLR/src/test.cpp
@@ -26,6 +26,8 @@ namespace F = torch::nn::functional;
 // ---------------
 void test(po::variables_map &vm, torch::Device &device, SimCLR &model, std::vector<transforms_Compose> &transform){
 
+    constexpr std::pair<float, float> output_range = {-1.0, 1.0};  // range of the value in output images
+
     // (0) Initialization and Declaration
     float ave_loss;
     double seconds, ave_time;
