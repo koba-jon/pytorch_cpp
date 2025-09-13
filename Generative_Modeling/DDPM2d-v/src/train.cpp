1d3db6ab858af5e5dcd7c063145a2a9d79d48af6
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2025-09-13 10:29:39 +0900

    Add files via upload

--- Generative_Modeling/DDPM2d-v/src/train.cpp
+++ Generative_Modeling/DDPM2d-v/src/train.cpp
@@ -154,6 +154,7 @@ void train(po::variables_map &vm, torch::Device &device, DDPM &model, DDPM &mode
     total_epoch = vm["epochs"].as<size_t>();
 
     // (2) Training per Epoch
+    mini_batch_size = 0;
     irreg_progress.restart(start_epoch - 1, total_epoch);
     for (epoch = start_epoch; epoch <= total_epoch; epoch++){
 
