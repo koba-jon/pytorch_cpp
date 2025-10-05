31101689b21cf18e76a63abc5f480b56fea459ca
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2025-10-06 03:04:05 +0900

    Update main.cpp

--- Multiclass_Classification/ViT/src/main.cpp
+++ Multiclass_Classification/ViT/src/main.cpp
@@ -69,7 +69,7 @@ po::options_description parse_arguments(){
 
         // (5) Define for Network Parameter
         ("lr", po::value<float>()->default_value(1e-4), "learning rate")
-        ("beta1", po::value<float>()->default_value(0.5), "beta 1 in Adam of optimizer method")
+        ("beta1", po::value<float>()->default_value(0.9), "beta 1 in Adam of optimizer method")
         ("beta2", po::value<float>()->default_value(0.999), "beta 2 in Adam of optimizer method")
         ("dim", po::value<size_t>()->default_value(1024), "dimensions of the embedded patch features")
         ("depth", po::value<size_t>()->default_value(6), "the number of layer in Transformer")
