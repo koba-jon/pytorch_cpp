159258288c2474d1a6d24a4422a57605b7703197
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2025-11-20 22:25:25 +0900

    Add files via upload

--- Multiclass_Classification/VGGNet/src/main.cpp
+++ Multiclass_Classification/VGGNet/src/main.cpp
@@ -67,7 +67,7 @@ po::options_description parse_arguments(){
         ("test_result_dir", po::value<std::string>()->default_value("test_result"), "test result directory : ./<test_result_dir>")
 
         // (5) Define for Network Parameter
-        ("lr", po::value<float>()->default_value(1e-4), "learning rate")
+        ("lr", po::value<float>()->default_value(1e-3), "learning rate")
         ("beta1", po::value<float>()->default_value(0.9), "beta 1 in Adam of optimizer method")
         ("beta2", po::value<float>()->default_value(0.999), "beta 2 in Adam of optimizer method")
         ("n_layers", po::value<size_t>(), "the number of layer in model")
