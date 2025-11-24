16df839c1911cc1a985cf76be508d1a5563cb8d4
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2025-11-24 13:33:29 +0900

    Add files via upload

--- Super_Resolution/SRGAN/src/main.cpp
+++ Super_Resolution/SRGAN/src/main.cpp
@@ -38,7 +38,7 @@ po::options_description parse_arguments(){
         ("help", "produce help message")
         ("dataset", po::value<std::string>(), "dataset name")
         ("hr_size", po::value<size_t>()->default_value(256), "high-resolution image width and height")
-        ("upscale", po::value<size_t>()->default_value(4), "upscale factor")
+        ("upscale", po::value<size_t>()->default_value(8), "upscale factor")
         ("nc", po::value<size_t>()->default_value(3), "input image channel : RGB=3, grayscale=1")
         ("loss", po::value<std::string>()->default_value("vanilla"), "vanilla (cross-entropy), lsgan (mse), etc.")
         ("gpu_id", po::value<int>()->default_value(0), "cuda device : 'x=-1' is cpu device")
