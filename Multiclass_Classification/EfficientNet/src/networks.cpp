7a8c5be105c833534e73cb000368313f746c09f7
Hiroki Kobayashi <56967584+koba-jon@users.noreply.github.com>
2025-12-04 19:56:25 +0900

    Add files via upload

--- Multiclass_Classification/EfficientNet/src/networks.cpp
+++ Multiclass_Classification/EfficientNet/src/networks.cpp
@@ -54,10 +54,10 @@ void StochasticDepthImpl::pretty_print(std::ostream& stream) const{
 // ----------------------------------------------------------------------
 // struct{Conv2dNormActivationImpl}(nn::Module) -> constructor
 // ----------------------------------------------------------------------
-Conv2dNormActivationImpl::Conv2dNormActivationImpl(const size_t in_nc, const size_t out_nc, const size_t kernel_size, const size_t stride, const size_t padding, const size_t groups, const bool SiLU){
+Conv2dNormActivationImpl::Conv2dNormActivationImpl(const size_t in_nc, const size_t out_nc, const size_t kernel_size, const size_t stride, const size_t padding, const size_t groups, const float eps, const float momentum, const bool SiLU){
     this->model = nn::Sequential(
         nn::Conv2d(nn::Conv2dOptions(/*in_channels=*/in_nc, /*out_channels=*/out_nc, /*kernel_size=*/kernel_size).stride(stride).padding(padding).groups(groups).bias(false)),
-        nn::BatchNorm2d(out_nc)  
+        nn::BatchNorm2d(nn::BatchNormOptions(out_nc).eps(eps).momentum(momentum))  
     );
     if (SiLU) this->model->push_back(nn::SiLU());
     register_module("Conv2dNormActivation", this->model);
@@ -113,16 +113,16 @@ torch::Tensor SqueezeExcitationImpl::forward(torch::Tensor x){
 // ----------------------------------------------------------------------
 // struct{MBConvImpl}(nn::Module) -> constructor
 // ----------------------------------------------------------------------
-MBConvImpl::MBConvImpl(const size_t in_nc, const size_t out_nc, const size_t kernel_size, const size_t stride, const size_t exp, const float dropconnect){
+MBConvImpl::MBConvImpl(const size_t in_nc, const size_t out_nc, const size_t kernel_size, const size_t stride, const size_t exp, const float eps, const float momentum, const float dropconnect){
 
     constexpr size_t reduce = 4;
     size_t mid = in_nc * exp;
     this->residual = ((stride == 1) && (in_nc == out_nc));
 
-    if (exp != 1) this->block->push_back(Conv2dNormActivation(in_nc, mid, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*SiLU=*/true));
-    this->block->push_back(Conv2dNormActivation(mid, mid, /*kernel_size=*/kernel_size, /*stride=*/stride, /*padding=*/kernel_size / 2, /*groups=*/mid, /*SiLU=*/true));
+    if (exp != 1) this->block->push_back(Conv2dNormActivation(in_nc, mid, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*eps=*/eps, /*momentum=*/momentum, /*SiLU=*/true));
+    this->block->push_back(Conv2dNormActivation(mid, mid, /*kernel_size=*/kernel_size, /*stride=*/stride, /*padding=*/kernel_size / 2, /*groups=*/mid, /*eps=*/eps, /*momentum=*/momentum, /*SiLU=*/true));
     this->block->push_back(SqueezeExcitation(mid, std::max(1, int(in_nc / reduce))));
-    this->block->push_back(Conv2dNormActivation(mid, out_nc, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*SiLU=*/false));
+    this->block->push_back(Conv2dNormActivation(mid, out_nc, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*eps=*/eps, /*momentum=*/momentum, /*SiLU=*/false));
     register_module("block", this->block);
     
     this->sd = StochasticDepth(dropconnect);
@@ -170,7 +170,7 @@ size_t MC_EfficientNetImpl::round_filters(size_t c, double width_mul){
 // struct{MC_EfficientNetImpl}(nn::Module) -> function{round_repeats}
 // ----------------------------------------------------------------------
 size_t MC_EfficientNetImpl::round_repeats(size_t r, double depth_mul){
-    return std::max(1, int(std::round(r * depth_mul)));
+    return std::max(1, int(std::ceil(r * depth_mul)));
 }
 
 
@@ -186,16 +186,16 @@ MC_EfficientNetImpl::MC_EfficientNetImpl(po::variables_map &vm){
 
     // (0.a) Setting for network's config
     std::string network = vm["network"].as<std::string>();
-    if (network == "B0") this->cfg = {1.0, 1.0, 224, 0.2};
-    else if (network == "B1") this->cfg = {1.0, 1.1, 240, 0.2};
-    else if (network == "B2") this->cfg = {1.1, 1.2, 260, 0.3};
-    else if (network == "B3") this->cfg = {1.2, 1.4, 300, 0.3};
-    else if (network == "B4") this->cfg = {1.4, 1.8, 380, 0.4};
-    else if (network == "B5") this->cfg = {1.6, 2.2, 456, 0.4};
-    else if (network == "B6") this->cfg = {1.8, 2.6, 528, 0.5};
-    else if (network == "B7") this->cfg = {2.0, 3.1, 600, 0.5};
-    else if (network == "B8") this->cfg = {2.2, 3.6, 672, 0.5};
-    else if (network == "L2") this->cfg = {4.3, 5.3, 800, 0.5};
+    if (network == "B0") this->cfg = {1.0, 1.0, 224, 0.2, 1e-5, 0.1, 0.2};
+    else if (network == "B1") this->cfg = {1.0, 1.1, 240, 0.2, 1e-5, 0.1, 0.2};
+    else if (network == "B2") this->cfg = {1.1, 1.2, 260, 0.3, 1e-5, 0.1, 0.2};
+    else if (network == "B3") this->cfg = {1.2, 1.4, 300, 0.3, 1e-5, 0.1, 0.2};
+    else if (network == "B4") this->cfg = {1.4, 1.8, 380, 0.4, 1e-5, 0.1, 0.2};
+    else if (network == "B5") this->cfg = {1.6, 2.2, 456, 0.4, 0.001, 0.01, 0.2};
+    else if (network == "B6") this->cfg = {1.8, 2.6, 528, 0.5, 0.001, 0.01, 0.2};
+    else if (network == "B7") this->cfg = {2.0, 3.1, 600, 0.5, 0.001, 0.01, 0.2};
+    else if (network == "B8") this->cfg = {2.2, 3.6, 672, 0.5, 0.001, 0.01, 0.2};
+    else if (network == "L2") this->cfg = {4.3, 5.3, 800, 0.5, 0.001, 0.01, 0.2};
     else{
         std::cerr << "Error : The type of network is " << network << '.' << std::endl;
         std::cerr << "Error : Please choose B0, B1, B2, B3, B4, B5, B6, B7, B8 or L2." << std::endl;
@@ -209,7 +209,7 @@ MC_EfficientNetImpl::MC_EfficientNetImpl(po::variables_map &vm){
 
     // (1) Stem layer
     stem_nc = this->round_filters(stem_feature, this->cfg.width_mul);
-    this->features->push_back(Conv2dNormActivation(vm["nc"].as<size_t>(), stem_nc, /*kernel_size=*/3, /*stride=*/2, /*padding=*/1, /*groups=*/1, /*SiLU=*/true));
+    this->features->push_back(Conv2dNormActivation(vm["nc"].as<size_t>(), stem_nc, /*kernel_size=*/3, /*stride=*/2, /*padding=*/1, /*groups=*/1, /*eps=*/this->cfg.eps, /*momentum=*/this->cfg.momentum, /*SiLU=*/true));
 
     // (2.a) Bone layer
     total_blocks = 0;
@@ -225,8 +225,8 @@ MC_EfficientNetImpl::MC_EfficientNetImpl(po::variables_map &vm){
         out_nc = this->round_filters(bcfg[i].c, this->cfg.width_mul);
         for (size_t j = 0; j < repeats; j++){
             stride = (j == 0) ? bcfg[i].s : 1;
-            dropconnect = this->cfg.dropout * (double)block_idx / (double)std::max(1, int(total_blocks));
-            this->features->push_back(MBConvImpl(in_nc, out_nc, bcfg[i].k, stride, bcfg[i].exp, dropconnect));
+            dropconnect = this->cfg.stochastic_depth_prob * (double)block_idx / (double)std::max(1, int(total_blocks));
+            this->features->push_back(MBConvImpl(in_nc, out_nc, bcfg[i].k, stride, bcfg[i].exp, this->cfg.eps, this->cfg.momentum, dropconnect));
             in_nc = out_nc;
             block_idx++;
         }
@@ -234,7 +234,7 @@ MC_EfficientNetImpl::MC_EfficientNetImpl(po::variables_map &vm){
 
     // (3) Head layer
     head_nc = this->round_filters(head_feature, this->cfg.width_mul);
-    this->features->push_back(Conv2dNormActivation(in_nc, head_nc, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*SiLU=*/true));
+    this->features->push_back(Conv2dNormActivation(in_nc, head_nc, /*kernel_size=*/1, /*stride=*/1, /*padding=*/0, /*groups=*/1, /*eps=*/this->cfg.eps, /*momentum=*/this->cfg.momentum, /*SiLU=*/true));
     register_module("features", this->features);
 
     // (4) Global Average Pooling
